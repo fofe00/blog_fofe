d880ca26c5d8608236eeb558faefac81b5890d09
LONFO FOFE <b.fofe@yahoo.com>
2020-07-02 07:50:38 +0100

    add modal login

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,19 +6,45 @@
 </div>
 <div class="ui modal ">
     <i class="close icon"></i>
-    <div class=" header"> Login</div>
+    <div class="">
+        <img class="image ui small centered" src="{{ asset('img/devWus.png') }}">
+    </div>
     <div class="content">
-        <form class="ui form">
-            <div class="field">
-                <label>Email</label>
-                <input type="email" name="email" placeholder="Email">
+        <div class="ui placeholder segment">
+            <div class="ui two column very relaxed stackable grid">
+                <div class="column">
+                    <div class="ui form">
+                        <div class="field">
+                            <label>Email</label>
+                            <div class="ui left icon input">
+                                <input type="text" placeholder="email">
+                                <i class="user icon"></i>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label>Password</label>
+                            <div class="ui left icon input">
+                                <input type="password">
+                                <i class="lock icon"></i>
+                            </div>
+                        </div>
+                        <div class="ui blue submit button">Login</div>
+                    </div>
+                </div>
+                <div class="middle aligned column">
+                    <div class="ui big button">
+                        <i class="signup icon"></i>
+                        Sign Up
+                    </div>
+                </div>
+                <div class="column">
+                    oki
+                </div>
             </div>
-            <div class="field">
-                <label>Password</label>
-                <input type="password" name="pwd" placeholder="Password">
+            <div class="ui vertical divider">
+                Or
             </div>
-            <button class="ui button primary" type="submit">Login</button>
-        </form>
+        </div>
     </div>
 </div>
 @include("partials._tfooter")
